2ada852a122ef67021dc447a6db23bf9cc284be8
scruz <sebastian.cruz@bibamgroup.com>
2018-08-06 10:25:47 -0300

    chore: maven project creation

---
+++ src/main/java/org/play/ground/atomic/App.java
@@ -0,0 +1,13 @@
+package org.play.ground.atomic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
