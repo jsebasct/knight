2f3382213d3df2361ae8f1b2be17062afa617dac
scruz <sebastian.cruz@bibamgroup.com>
2018-08-13 17:52:54 -0300

    bug: cant recover information

--- src/main/java/org/play/ground/atomic/App.java
+++ src/main/java/org/play/ground/atomic/App.java
@@ -1,13 +1,45 @@
 package org.play.ground.atomic;
 
+import java.util.Iterator;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaQuery;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.play.ground.atomic.data.Employee;
+import org.play.ground.atomic.data.HibernateUtil;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    public void insertEmployee() {
+        Employee fordFocus = new Employee("Nicolas", "Martinez");
+        Employee hondaCivic = new Employee("Mati", "Martinez");
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        Transaction tx = session.beginTransaction();
+        session.save(fordFocus);
+        session.save(hondaCivic);
+        tx.commit();
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Start" );
+
+        App app = new App();
+        app.insertEmployee();
+
+        HibernateUtil.shutdown();
     }
+
 }
