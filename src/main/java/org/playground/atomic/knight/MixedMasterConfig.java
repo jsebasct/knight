785cd2f623dea866d82f2df3d02c970a61545977
jscruz <jsebas.ct@gmail.com>
2018-08-24 00:24:29 -0300

    feat: mixed config complete

--- src/main/java/org/playground/atomic/knight/MixedMasterConfig.java
+++ src/main/java/org/playground/atomic/knight/MixedMasterConfig.java
@@ -2,8 +2,10 @@ package org.playground.atomic.knight;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan(basePackages = {"org.playground.atomic.knight"})
+@ImportResource("/knight_beans.xml")
 public class MixedMasterConfig {
 }
